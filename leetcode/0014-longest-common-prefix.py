b1320cf417e28798856af7309d0d4ecd6c816b94
Vivek Raman <me@vivekraman.dev>
2026-03-10 11:46:35 -0700

    14 - Longest Common Prefix - revisited

--- leetcode/0014-longest-common-prefix.py
+++ leetcode/0014-longest-common-prefix.py
@@ -2,15 +2,28 @@ from typing import List
 
 
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        lcp = ''
-        for index, char in enumerate(strs[0]):
-            for st in strs:
-                if index >= len(st) or st[index] != char:
-                    return lcp
-            lcp += char
-        return lcp
+
+  def longestCommonPrefix_bruteForce(self, strs: List[str]) -> str:
+    lcp = ''
+    for index, char in enumerate(strs[0]):
+      for st in strs:
+        if index >= len(st) or st[index] != char:
+          return lcp
+      lcp += char
+    return lcp
+
+  def longestCommonPrefix(self, strs: List[str]) -> str:
+    strs = sorted(strs)
+    l = strs[0]
+    r = strs[-1]
+    lcp = ''
+    for i in range(min(len(l), len(r))):
+      if l[i] == r[i]:
+        lcp += l[i]
+      else:
+        break
+    return lcp
 
 
 if __name__ == '__main__':
-    print(Solution().longestCommonPrefix(["ab","a"]))
+  print(Solution().longestCommonPrefix(["cluster", "clue", "clumsy"]))
